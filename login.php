219d0e92613e17e570ee1a8a26fb364703d0ff58
Lana Karmaoui <lana.karmaoui@gmail.com>
2024-04-26 11:04:25 +0200

    ajout de fichier et correction

--- login.php
+++ login.php
@@ -13,8 +13,8 @@
     $title = "Dauphine";
 
     include_once("block/header.php");
- // Verifier si le form a bien été rempli.
-    
+    // Verifier si le form a bien été rempli.
+
     if (
         $_SERVER["REQUEST_METHOD"] === "POST" &&
         isset($_POST["username"], $_POST["password"])
@@ -22,65 +22,63 @@
 
 
         //Tableau pour les erreurs
-    $errors = [];
+        $errors = [];
 
-    //Connection avec la base de données 
+        //Connection avec la base de données 
 
-    $pdo = connectDB();
+        $pdo = connectDB();
 
-    // Chercher l'utilisateur 
+        // Chercher l'utilisateur 
 
-    $response = $pdo->prepare("SELECT username, password FROM utilisateur WHERE username = :username AND password = :password");
-    $response->execute([
-        ":username" => $_POST["username"],
-        ":password" => $_POST["password"]
-    ]);
+        $response = $pdo->prepare("SELECT username, password FROM utilisateur WHERE username = :username AND password = :password");
+        $response->execute([
+            ":username" => $_POST["username"],
+            ":password" => $_POST["password"]
+        ]);
 
-    //Je recupère le resultat ou l'erreur
-    $user = $response->fetch();
+        //Je recupère le resultat ou l'erreur
+        $user = $response->fetch();
 
-    if($user !== false){
-        //Connexion réussie
-         session_start();
-         $_SESSION["username"] = $_POST["username"];
+        if ($user !== false) {
+            //Connexion réussie
+            session_start();
+            $_SESSION["username"] = $_POST["username"];
 
-         header("Location: https://localhost/pokedex/admin/index.php");
-
-     }else {
-         $errors["global"] = "Identifiants invalides";
-     }
-
+            header("Location: https://localhost/pokedex/admin/index.php");
+        } else {
+            $errors["global"] = "Identifiants invalides";
+        }
     } else {
-     $errors["global"] = "Identifiants manquants";
+        $errors["global"] = "Identifiants manquants";
     }
- 
+
 
 
     ?>
-// Formulaire de connection 
+    // Formulaire de connection
 
-<div class="container">
+    <div class="container">
 
-<h1 class="text-center m-3"><?php echo ($title) ?></h1>
+        <h1 class="text-center m-3"><?php echo ($title) ?></h1>
 
-<form method="POST" action="login.php">
+        <form method="POST" action="login.php">
 
-    <label for="username">Username</label>
-    <input type="text" name="username" id="username">
-    <label for="password">Password</label>
-    <input type="text" name="password" id="password">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username">
+            <label for="password">Password</label>
+            <input type="text" name="password" id="password">
 
-    <?php
-    if (isset($errors["global"])) {
-        echo ("<p class='text-danger'>" .
-            $errors["global"] . "</p>");
-    }
-    ?>
+            <?php
+            if (isset($errors["global"])) {
+                echo ("<p class='text-danger'>" .
+                    $errors["global"] . "</p>");
+            }
+            ?>
 
-    <input type="submit" value="Valider">
-</form>
+            <input type="submit" value="Valider">
+        </form>
 
-</div>
+    </div>
 
 
 
