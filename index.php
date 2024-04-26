feaa208b037e4d3627c5b4b845fec5e7a11d1801
Lana Karmaoui <lana.karmaoui@gmail.com>
2024-04-26 10:59:19 +0200

    ajout login et mdp hash

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 $title = "Dauphine";
 
 include_once("block/header.php");
+
+// Procédure de hashage du password pour l'utilisateur jose, je vais montré le password hashé, crée l'utilisateur avec.
+$passHash = password_hash("jose", PASSWORD_DEFAULT);
+echo ($passHash);
+var_dump(password_verify("jose", $passHash));
 ?>
 
 <div class="container">
@@ -9,6 +14,13 @@ include_once("block/header.php");
     <h1 class="text-center"><?php echo ($title ?? "Default Title") ?></h1>
 
 </div>
+
+<h1>Bienvenue</h1>
+
+
+
+
+
 
 <?php
 include_once("block/footer.php");
---
+++ login.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+
+    <?php
+    $title = "Dauphine";
+
+    include_once("block/header.php");
+ // Verifier si le form a bien été rempli.
+    
+    if (
+        $_SERVER["REQUEST_METHOD"] === "POST" &&
+        isset($_POST["username"], $_POST["password"])
+    ) {
+
+
+        //Tableau pour les erreurs
+    $errors = [];
+
+    //Connection avec la base de données 
+
+    $pdo = connectDB();
+
+    // Chercher l'utilisateur 
+
+    $response = $pdo->prepare("SELECT username, password FROM utilisateur WHERE username = :username AND password = :password");
+    $response->execute([
+        ":username" => $_POST["username"],
+        ":password" => $_POST["password"]
+    ]);
+
+    //Je recupère le resultat ou l'erreur
+    $user = $response->fetch();
+
+    if($user !== false){
+        //Connexion réussie
+         session_start();
+         $_SESSION["username"] = $_POST["username"];
+
+         header("Location: https://localhost/pokedex/admin/index.php");
+
+     }else {
+         $errors["global"] = "Identifiants invalides";
+     }
+
+    } else {
+     $errors["global"] = "Identifiants manquants";
+    }
+ 
+
+
+    ?>
+// Formulaire de connection 
+
+<div class="container">
+
+<h1 class="text-center m-3"><?php echo ($title) ?></h1>
+
+<form method="POST" action="login.php">
+
+    <label for="username">Username</label>
+    <input type="text" name="username" id="username">
+    <label for="password">Password</label>
+    <input type="text" name="password" id="password">
+
+    <?php
+    if (isset($errors["global"])) {
+        echo ("<p class='text-danger'>" .
+            $errors["global"] . "</p>");
+    }
+    ?>
+
+    <input type="submit" value="Valider">
+</form>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php
+    include_once("block/footer.php");
+    ?>
+
+</body>
+
+</html>
